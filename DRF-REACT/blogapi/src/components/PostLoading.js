2d318a0f774591200ef434f8ca51e3d33f68ef48
marciodegan <marciodegan@yahoo.com>
2020-12-07 21:27:14 -0300

    uncommented if statement

--- DRF-REACT/blogapi/src/components/PostLoading.js
+++ DRF-REACT/blogapi/src/components/PostLoading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function PostLoading(Component) {
     return function PostLoadingComponent({ isLoading, ...props }) {
-        // if (!isLoading) return <Component {...props} />;
+        if (!isLoading) return <Component {...props} />;
         return (
             <p style={{ fontSize: '25px' }}>
                 Aguardando carregar posts!...
