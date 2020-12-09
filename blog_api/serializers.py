140ba8e4ec26cfc5ba37654ddfbc9f26ec25eb92
marciodegan <marciodegan@yahoo.com>
2020-12-09 11:58:01 -0300

    just added category field to the serializer

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'title', 'author', 'excerpt', 'content', 'status')
+        fields = ('id', 'title', 'slug', 'author', 'excerpt', 'content', 'status', 'category')
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
