b13fdbc576782d366547d9c3f273f0dbb5b07d0f
marciodegan <marciodegan@yahoo.com>
2020-12-09 12:00:26 -0300

    added slug route

--- DRF-REACT/blogapi/src/index.js
+++ DRF-REACT/blogapi/src/index.js
@@ -9,22 +9,27 @@ import Footer from './components/footer';
 import Register from './components/register';
 import Login from './components/login';
 import Logout from './components/logout';
+import Single from './components/single';
 
 const routing = (
-  <Router>
-    <React.StrictMode>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/logout" component={Logout} />
-      </Switch>
-      <Footer />
-    </React.StrictMode>  
-  </Router>
+	<Router>
+		<React.StrictMode>
+			<Header />
+			<Switch>
+				<Route exact path="/" component={App} />
+				<Route path="/register" component={Register} />
+				<Route path="/login" component={Login} />
+				<Route path="/logout" component={Logout} />
+				<Route path="/post/:slug" component={Single} />
+			</Switch>
+			<Footer />
+		</React.StrictMode>
+	</Router>
 );
 
 ReactDOM.render(routing, document.getElementById('root'));
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
