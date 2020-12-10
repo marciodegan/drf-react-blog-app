6826834a1f6806a1fb67041ff0d7700ccb66df39
marciodegan <marciodegan@yahoo.com>
2020-12-10 16:54:00 -0300

    added CRUD urls

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostList, PostDetail, PostListDetailFilter
+from .views import PostList, PostDetail, PostListDetailFilter, CreatePost, EditPost, AdminPostDetail, DeletePost
 from rest_framework.routers import DefaultRouter
 
 app_name = 'blog_api'
@@ -16,10 +16,12 @@ app_name = 'blog_api'
 # ]
 
 urlpatterns = [
-    path('posts/', PostDetail.as_view(), name='detailcreate'),
-    # path('posts/<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('', PostList.as_view(), name='listpost'),
+    path('post/<str:pk>/', PostDetail.as_view(), name='detailpost'),
     path('search/', PostListDetailFilter.as_view(), name='postsearch'),
-    # path('search/', PostSearch.as_view(), name='postsearch'),
-    path('', PostList.as_view(), name='listcreate'),
     
+    path('admin/create/', CreatePost.as_view(), name='createpost'),
+    path('admin/edit/postdetail/<int:pk>/', AdminPostDetail.as_view(), name='admindetailpost'),
+    path('admin/edit/<int:pk>/', EditPost.as_view(), name='editpost'),
+    path('admin/delete/<int:pk>/', DeletePost.as_view(), name='deletepost')
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -3,9 +3,6 @@ from users.models import CustomUser
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    """
-    Currently unused in preference of the below.
-    """
     email = serializers.EmailField(required=True)
     user_name = serializers.CharField(required=True)
     password = serializers.CharField(min_length=8, write_only=True)
