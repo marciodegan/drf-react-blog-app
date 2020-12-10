d79b59b36723cb0de78c298652ce082943d9a1e6
marciodegan <marciodegan@yahoo.com>
2020-12-10 17:26:24 -0300

    update

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from blog.models import Post
 from django.conf import settings
 
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
